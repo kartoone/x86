48d9e38830e6aee3ab669a2e768a1b8650845b0f
Brian Toone <brtoone@samford.edu>
2021-10-28 11:36:41 -0500

    updated part4 code with more comments

--- part4.s
+++ part4.s
@@ -2,7 +2,7 @@
 prompt: .string "Enter a number: "  # printf("Enter a number:");
 answer: .string "The sum of the numbers between %d (inclusive) and %d (inslusive) = %d\n"    # printf("%d + %d = %d\n", ...)
 scanformat: .string "%dl"            # scanf("%dl", ...)
-iprintformat: .string "%d\n"        # printf("%d\n", i);
+iprintformat: .string "i:%d\n"        # printf("%d\n", i);
 
 .section .data
 num1: .quad 0
@@ -33,32 +33,45 @@ main:
   call scanf@PLT
 
   # get the numbers from memory into registers we need to use for call to printf
-  movq num1(%rip), %r12   # i = num1
-  movq num2(%rip), %r13   # num2 (our stopping point will be in %r13)
-  movq $0, %r14           # total = 0
+  movq num1(%rip), %r12   # %r12: num1
+  movq num2(%rip), %r13   # %r13: num2
+  movq %r12, %r14         # %r14: i ... note that this is essentially: i=num1;
+  movq $0, %r15           # %r15: total ... note that this is essentially: total=0;
 
-whileloop: cmp %r13, %r12 # note that the comparison is BACKWARDS %r12 > %r13 when written cmp %r13, %r12
-  jg finished             # only takes the jump if %r12 > %r13
+whileloop: cmp %r13, %r14 # note that the comparison is BACKWARDS %r14 > %r13 when written cmp %r13, %r14
+  jg finished             # only takes the jump if %r14 > %r13
 
-  # prepare arguments for printf to current value
+  # If we make it to here, 
+  #		then our code didn't jump 
+  #		so we are NOW inside the body of the loop
+  #
+  # FOR YOU TO DO HERE INSIDE THE LOOP: 
+  #    add current value of "i" to your current "total" 
+  #	   HINT: think about which registers are being used to store the values for i and total 
+  #    HINT: also think about how the "add" instruction works
+  #    FINAL HINT: you are only adding a single "add" instruction below this comment
+
+  # WHAT I HAVE DONE BELOW IS ALSO STILL INSIDE THE BODY OF THE LOOP:
+  #  prepared registers and call printf to display the current loop count (i.e., "i")
   leaq iprintformat(%rip), %rdi
-  movq %r12, %rsi
-  call printf             # printf("%d\n", i);
-  inc %r12                # i++
-  jmp whileloop
+  movq %r14, %rsi
+  call printf             # printf("i:%d\n", i);
+  inc %r14                # i++
+  jmp whileloop			  # jump back to beginning of loop ... equivalent to a closing "}"
 
 finished:
   # display the answer
   # 1. put format string into %rdi (already done for you below)
   leaq answer(%rip), %rdi
 
-  # 2. put number 1 back into %rsi (you have to do this - movq number 1 from memory)
+  # 2. put num1 into %rsi (what register is it currently in?) HINT: movq instruction
 
-  # 3. put number 2 into %rdx (you have to do this - movq number 2 from memory)
+  # 3. put num2 into %rdx (what register is it currently in?) HINT: movq instruction
 
-  # 4. move the answer (your total) %r14 into %rcx
+  # 4. move the answer into %rcx (your total - what register is it in?) HINT: movq instruction
 
-  call printf@PLT
+  # UNCOMMENT THE CALL TO PRINTF BELOW once you have done #2, #3, and #4
+  #call printf@PLT
 
   mov $0, %rax      # set our return value register to indicate we exited normally
   pop %rbp          # realign the stack for the OS
