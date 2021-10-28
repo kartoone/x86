7cc33fc4d9bae8c9661075a622c7dd69e3abf098
Brian Toone <brtoone@samford.edu>
2021-10-28 05:52:46 -0500

    initial work with hello.s

--- hello.s
+++ hello.s
@@ -9,6 +9,8 @@ main:
   leaq str1(%rip), %rdi
   mov $4, %rsi
   call printf@PLT   # printf("hello, assembly, %d\n", 4);
+
+  # part 1 - add two hard coded numbers together and return
   mov $1, %rbx
   mov $2, %rax
   add %rbx, %rax
