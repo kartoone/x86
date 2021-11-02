5d51398b4b5526ff365c4088306070f3fd4002ce
kartoone <brtoone@samford.edu>
2021-11-02 06:22:17 -0500

    optimized floating point example

--- myfloat.s
+++ myfloat.s
@@ -2,15 +2,15 @@
 formats: .string "%f\n"
 pi: .double 3.141519
 
-.section text
+.text
 .globl main
 main:
 	push %rbp
 	leaq formats(%rip), %rdi
 	mov $1, %eax
 	movsd pi(%rip), %xmm0
-	call printf
-	move $0, $rax
+	call printf@PLT
+	movq $0, %rax
 	pop %rbp
 	ret
 	
